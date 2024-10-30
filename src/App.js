0c6a0645cbf584b6232c3fdd16d45a94e775509c
alphamuhia <muhiaalpha@gmail.com>
2024-10-30 12:37:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import Days from "./components/Days";
-import Tasks from "./components/Tasks";
+// import Days from "./components/Days";
+// import Tasks from "./components/Tasks";
 
 function App() {
   return (
     <div className="App">
-      <Days />
+      {/* <Days />
       <Tasks />
-      <h1>Collaboration example</h1>
+      <h1>Collaboration example</h1> */}
       <nav>
         <link href="/">home</link>
         <link href="/">about</link>
       </nav>
+      <div>
+        <ul>
+          <li>go shoping</li>
+        </ul>
+      </div>
     </div>
   );
 }
