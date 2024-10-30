7c85810ba6eaa7937d0258056e7962d9bc7ef70a
alphamuhia <muhiaalpha@gmail.com>
2024-10-30 12:41:49 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
-// import Days from "./components/Days";
-// import Tasks from "./components/Tasks";
+import Days from "./components/Days";
+import Tasks from "./components/Tasks";
+import Test from "./Test";
 
 function App() {
   return (
     <div className="App">
-      {/* <Days />
+      <Days />
       <Tasks />
-      <h1>Collaboration example</h1> */}
-      <nav>
-        <link href="/">home</link>
-        <link href="/">about</link>
-      </nav>
-      <div>
-        <ul>
-          <li>go shoping</li>
-        </ul>
-      </div>
+      <Test />
+      <h1>Collaboration example</h1>
     </div>
   );
 }
---
+++ src/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+        <p>join
+            
+        </p>
+    </div>
+  )
+}
+
+export default Test
